9cefc2306b2f922f99953595f9f2ca5d15cb4bbb
avperrotta <avperrotta@gmail.com>
2014-08-01 08:49:00 +0100

    java stuff

---
+++ java/classes/src/httpGet.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author perrotta
+ */
+import com.cycling74.max.*;
+
+public class httpGet extends MaxObject {
+
+  public void bang() {
+    outlet(0, "Congratulations!");
+  }
+}
